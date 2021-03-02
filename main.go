8bb2e5ad93e8ef0fd364a9d069ae2fe1887021d0
Jeong Hyunseok <hsjeong@irisid.com>
2021-03-02 15:17:06 +0900

    success to get date and confirmed person

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -8,6 +9,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	_ "embed"
@@ -46,10 +48,11 @@ var netClient = &http.Client{
 
 const (
 	openAPIURL = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson"
+	dateFormat = "20060102"
 )
 
 func get3WeeksRange() (string, string) {
-	dateFormat := "20060102"
+
 	cTime := time.Now()
 	endDate := cTime.Format(dateFormat)
 	startDate := cTime.AddDate(0, 0, -23).Format(dateFormat) // I need 21 days, but I have 23 days just in case
@@ -72,10 +75,10 @@ func getCoronaData() ([]byte, error) {
 	q.Add("numOfRows", "25") // I will have max 23 days result
 	q.Add("startCreateDt", startDate)
 	q.Add("endCreateDt", endDate)
-	req.URL.RawQuery = q.Encode()
 
-	fmt.Println("query:", q)
-	fmt.Println("url:", req.URL.RequestURI())
+	fmt.Println("req.URL.String():", req.URL.String())
+	req.URL.RawQuery = q.Encode() // this make added query to attached
+	fmt.Println("req.URL.String():", req.URL.String())
 
 	resp, err := netClient.Do(req)
 	if err != nil {
@@ -96,6 +99,96 @@ func getCoronaData() ([]byte, error) {
 	return bodyBytes, nil
 }
 
+// research.swtch.com
+type Response struct {
+	XMLName xml.Name `xml:"response"`
+	Text    string   `xml:",chardata"`
+	Header  struct {
+		Text       string `xml:",chardata"`
+		ResultCode string `xml:"resultCode"`
+		ResultMsg  string `xml:"resultMsg"`
+	} `xml:"header"`
+	Body struct {
+		Text  string `xml:",chardata"`
+		Items struct {
+			Text string `xml:",chardata"`
+			Item []struct {
+				Text           string `xml:",chardata"`
+				AccDefRate     string `xml:"accDefRate"`
+				AccExamCnt     string `xml:"accExamCnt"`
+				AccExamCompCnt string `xml:"accExamCompCnt"`
+				CareCnt        string `xml:"careCnt"`
+				ClearCnt       string `xml:"clearCnt"`
+				CreateDt       string `xml:"createDt"`
+				DeathCnt       string `xml:"deathCnt"`
+				DecideCnt      string `xml:"decideCnt"`
+				ExamCnt        string `xml:"examCnt"`
+				ResutlNegCnt   string `xml:"resutlNegCnt"`
+				Seq            string `xml:"seq"`
+				StateDt        string `xml:"stateDt"`
+				StateTime      string `xml:"stateTime"`
+				UpdateDt       string `xml:"updateDt"`
+			} `xml:"item"`
+		} `xml:"items"`
+		NumOfRows  string `xml:"numOfRows"`
+		PageNo     string `xml:"pageNo"`
+		TotalCount string `xml:"totalCount"`
+	} `xml:"body"`
+}
+
+type Item struct {
+	Text           string `xml:",chardata"`
+	AccDefRate     string `xml:"accDefRate"`
+	AccExamCnt     string `xml:"accExamCnt"`
+	AccExamCompCnt string `xml:"accExamCompCnt"`
+	CareCnt        string `xml:"careCnt"`
+	ClearCnt       string `xml:"clearCnt"`
+	CreateDt       string `xml:"createDt"`
+	DeathCnt       string `xml:"deathCnt"`
+	DecideCnt      string `xml:"decideCnt"`
+	ExamCnt        string `xml:"examCnt"`
+	ResutlNegCnt   string `xml:"resutlNegCnt"`
+	Seq            string `xml:"seq"`
+	StateDt        string `xml:"stateDt"`
+	StateTime      string `xml:"stateTime"`
+	UpdateDt       string `xml:"updateDt"`
+}
+
+type CoronaDailyData struct {
+	Date     string
+	AddCount string
+}
+
+func getAddCount(today Item, yday Item) string {
+
+	tCareCnt, err := strconv.Atoi(today.CareCnt)
+	if err != nil {
+		return "-1"
+	}
+	yCareCnt, err := strconv.Atoi(yday.CareCnt)
+	if err != nil {
+		return "-1"
+	}
+	tClearCnt, err := strconv.Atoi(today.ClearCnt)
+	if err != nil {
+		return "-1"
+	}
+	yClearCnt, err := strconv.Atoi(yday.ClearCnt)
+	if err != nil {
+		return "-1"
+	}
+	tDeathCnt, err := strconv.Atoi(today.DeathCnt)
+	if err != nil {
+		return "-1"
+	}
+	yDeathCnt, err := strconv.Atoi(yday.DeathCnt)
+	if err != nil {
+		return "-1"
+	}
+
+	return strconv.Itoa(tCareCnt + tClearCnt + tDeathCnt - yCareCnt - yClearCnt - yDeathCnt)
+}
+
 func weeklyHandler(w http.ResponseWriter, r *http.Request) {
 
 	// if the last creation of the html is over 2 min
@@ -106,7 +199,33 @@ func weeklyHandler(w http.ResponseWriter, r *http.Request) {
 		// need return code
 		return
 	}
-	fmt.Println(string(b))
+	// fmt.Println(string(b))
+
+	var resp Response
+	if err := xml.Unmarshal(b, &resp); err != nil {
+		log.Println(err)
+		return
+	}
+
+	var data []CoronaDailyData
+	for i := range resp.Body.Items.Item {
+
+		if i == len(resp.Body.Items.Item)-1 {
+			continue
+		}
+		t, err := time.Parse(dateFormat, resp.Body.Items.Item[i].StateDt)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		var d CoronaDailyData
+		d.Date = t.AddDate(0, 0, -1).Format(dateFormat)
+		d.AddCount = getAddCount(resp.Body.Items.Item[i], resp.Body.Items.Item[i+1])
+		data = append(data, d)
+	}
+
+	// fmt.Printf("data: %+v\n", data)
 
 	// create a new bar instance
 	bar := charts.NewBar()
